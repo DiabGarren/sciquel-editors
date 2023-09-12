ff8f46c0403e8f9cb2fcf65b48c91f5f6232a803
DiabGarren <garrendiab@gmail.com>
2023-09-13 00:39:44 +0200

    Started cover image editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+import ImageContainer from "@/components/cover_image/ImageContainer";
 import TableContainer from "@/components/table_graph/TableContainer";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { type Crop, type PixelCrop } from "react-image-crop";
 
 export default function Home() {
     const [manual, setManual] = useState(false);
@@ -8,6 +10,14 @@ export default function Home() {
     const [cols, setCols] = useState(2);
     let [cell, setCell] = useState([]);
     const [graphType, setGraphType] = useState("bar");
+
+    const [image, setImage] = useState("");
+    const imgRef = useRef<HTMLImageElement>(null);
+    const previewCanvasRef = useRef<HTMLCanvasElement>(null);
+    const [crop, setCrop] = useState<Crop>();
+    const [completedCrop, setCompletedCrop] = useState<PixelCrop>();
+    const [scale, setScale] = useState(1);
+    const [rotate, setRotate] = useState(0);
 
     const graphProps = {
         manual,
@@ -22,11 +32,27 @@ export default function Home() {
         setType: setGraphType,
     };
 
+    const imageProps = {
+        image,
+        setImage,
+        imgRef,
+        previewCanvasRef,
+        crop,
+        setCrop,
+        completedCrop,
+        setCompletedCrop,
+        scale,
+        setScale,
+        rotate,
+        setRotate,
+    };
+
     return (
         <main className="m-[50px]">
-            <TableContainer
+            {/* <TableContainer
                 {...graphProps}
-            />
+            /> */}
+            <ImageContainer {...imageProps} />
         </main>
     );
 }
---
+++ components/cover_image/ImageContainer.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import { useEffect } from "react";
+import ImagePreview from "./ImagePreview";
+import ReactCrop from "react-image-crop";
+
+export default function ImageContainer(props: any) {
+    const fileSelect = (event: any) => {
+        if (event.target.files && event.target.files.length > 0) {
+            props.setCrop(undefined);
+
+            const file = new FileReader();
+            file.addEventListener("load", () => {
+                props.setImage(file.result?.toString() || "");
+            });
+            file.readAsDataURL(event.target.files[0]);
+        }
+    };
+
+    useEffect(() => {
+        const time = setTimeout(() => {
+            (async () => {
+                if (
+                    props.completedCrop?.width &&
+                    props.completeCrop?.height &&
+                    props.imgRef.current &&
+                    props.previewCanvasRef.current
+                ) {
+                    ImagePreview(
+                        props.imgRef.current,
+                        props.previewCanvasRef.current,
+                        props.completedCrop
+                    );
+                }
+            }).apply(undefined, props.completedCrop);
+        }, 100);
+
+        return () => {
+            clearTimeout(time);
+        };
+    }, props.completedCrop);
+
+    return (
+        <div>
+            <input type="file" accept="image/*" onChange={fileSelect} />
+            {!!props.image && (
+                <ReactCrop
+                    crop={props.crop}
+                    onChange={(_, percentCrop) => props.setCrop(percentCrop)}
+                    minHeight={200}
+                >
+                    {/* <Image ref={props.imgRef} src={props.image} alt="" /> */}
+                    <img src={props.image} alt="" />
+                </ReactCrop>
+            )}
+            {!!props.completeCrop && (
+                <>
+                    <div>
+                        <canvas ref={props.previewCanvasRef} className="border-1 fit-contain"/>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
