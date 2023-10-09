8f6d1026e376acf3869ba1ed9218da8fa76cfcaf
diabGarren <garrendiab@gmail.com>
2023-10-09 14:27:50 +0200

    Changed cover image size

--- components/cover_image/preview/coverImagePreivew.tsx
+++ components/cover_image/preview/coverImagePreivew.tsx
@@ -7,7 +7,7 @@ export default function CoverImagePreview(props: any) {
     }
     return (
         <div
-            className="w-[900px] mx-[auto] h-[500px] bg-center bg-cover bg-no-repeat flex flex-row items-end"
+            className="w-[1200px] mx-[auto] h-[500px] bg-center bg-cover bg-no-repeat flex flex-row items-end"
             style={{ backgroundImage: imageUrl, borderRadius: "0 0 5px 5px" }}
         >
             <div className="ml-[25px] mb-[15px] text-white">
