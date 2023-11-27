68bdef6655263d0cfbf67d35b2815a3415b9f1c1
diabGarren <garrendiab@gmail.com>
2023-11-27 18:20:25 +0200

    Completed algorithm for adding dict word style

--- components/section_container/text_container/preview/textContainer.tsx
+++ components/section_container/text_container/preview/textContainer.tsx
@@ -1,5 +1,4 @@
 import parse from "html-react-parser";
-import Popup from "reactjs-popup";
 
 export default function TextContainerPreview(props: any) {
     if (props.section[props.index].type === "sectionHeader") {
@@ -8,77 +7,46 @@ export default function TextContainerPreview(props: any) {
         );
     } else {
         if (props.dictionary.length > 0) {
-            // let text = <></>;
-
-            // props.dictionary.map((def: any) => {
-            //     if (def.word !== "") {
-            //         text = (
-            //             <>
-            //                 {parse(
-            //                     props.section[props.index].text.substring(
-            //                         0,
-            //                         props.section[props.index].text.indexOf(def.word)
-            //                     )
-            //                 )}
-            //                 <Popup
-            //                     trigger={
-            //                         <button className="text-teal-dark bg-teal-light-2 border border-teal-light-1 px-[2px] rounded">
-            //                             {def.word}
-            //                         </button>
-            //                     }>
-            //                     {def.word}
-            //                 </Popup>
-            //                 {parse(
-            //                     props.section[props.index].text.substring(
-            //                         props.section[props.index].text.indexOf(def.word) +
-            //                             def.word.length
-            //                     )
-            //                 )}
-            //             </>
-            //         );
-            //     }
-            // });
-
-            // return (
-            //     <div className="text-container break-words w-[500px] mx-auto ">{text}</div>
-            // );
-
             let text = props.section[props.index].text;
             const tags = [...text.matchAll(/<[^>]+>/g)];
-            console.log(tags);
+            let plainText = text.replace(/<[^>]+>/g, "†");
 
-            props.dictionary.map((def: any, index: number) => {
-                if (def.word !== "" && text.indexOf(def.word) !== -1) {
-                    if (
-                        text.indexOf(def.word) >= tags[0].index &&
-                        text.indexOf(def.word) <= tags[0].index + tags[0][0].length
-                    ) {
-                        text = `${text.substring(
-                            0,
-                            text.indexOf(def.word, tags[1].index + tags[1][0].length)
-                        )}<span className="text-teal-dark bg-teal-light-2 border border-teal-light-1 px-[2px] rounded">${
-                            def.word
+            const defIndexs = props.dictionary.map((def: any, index: number) => {
+                if (def.word === "") {
+                    return [-1, ""];
+                }
+                return [plainText.indexOf(def.word), def.word];
+            });
+            defIndexs.sort();
+
+            let newIndex = 0;
+            defIndexs.forEach((defIndex: any[], index: number) => {
+                if (defIndex[0] !== -1) {
+                    if (index > 0) {
+                        for (let i = 0; i < index; i++) {
+                            if (defIndexs[i][1].length > 0) {
+                                // Length of inserted span
+                                newIndex += 113 + i.toString().length;
+                            }
                         }
-                    <sup className="">[${index + 1}]</sup></span>${text.substring(
-                            text.indexOf(def.word, tags[1].index + tags[1][0].length) +
-                                def.word.length
-                        )}`;
-                    } else {
-                        text = `${text.substring(
-                            0,
-                            text.indexOf(def.word)
-                        )}<span className="text-teal-dark bg-teal-light-2 border border-teal-light-1 px-[2px] rounded">${
-                            def.word
-                        }
-                    <sup className="">[${index + 1}]</sup></span>${text.substring(
-                            text.indexOf(def.word) + def.word.length
-                        )}`;
                     }
+                    plainText =
+                        plainText.substring(0, defIndex[0] + newIndex) +
+                        `<span className="text-teal-dark bg-teal-light-2 border border-teal-light-1 px-[2px] rounded">${
+                            defIndex[1]
+                        }<sup>[${index + 1}]</sup></span>` +
+                        plainText.substring(defIndex[0] + newIndex + defIndex[1].length);
                 }
             });
 
+            tags.forEach((tag: any) => {
+                plainText = plainText.replace("†", tag[0]);
+            });
+
             return (
-                <div className="text-container break-words w-[500px] mx-auto ">{parse(text)}</div>
+                <div className="text-container break-words w-[500px] mx-auto ">
+                    {parse(plainText)}
+                </div>
             );
         }
         return (
