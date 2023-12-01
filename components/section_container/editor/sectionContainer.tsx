ed8bee04d304d8f56680575fe5050c506905a801
diabGarren <garrendiab@gmail.com>
2023-12-01 16:18:56 +0200

    Changed dropdown, added heading

--- components/section_container/editor/sectionContainer.tsx
+++ components/section_container/editor/sectionContainer.tsx
@@ -3,10 +3,22 @@ import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@
 import TextContainerEditor from "../text_container/editor/textContainer";
 import ImageContainerEditor from "../image_container/editor/imageContainer";
 import TableGraphEditor from "@/components/table_graph/editor/tableGraph";
+import Popup from "reactjs-popup";
 
+const sectionItems = [
+    { name: "Section Header", key: "sectionHeader" },
+    { name: "Text", key: "text" },
+    { name: "Image", key: "image" },
+    { name: "Table/graph", key: "tableGraph" },
+];
 export default function SectionContainerEditor(props: any) {
     return (
         <div className="border border-grey-light-1 rounded-lr p-[7px]">
+            <div className="border border-grey-light-1 rounded-box p-[15px] mb-[10px]">
+                <h3>
+                    {props.mediaType === "Article" ? props.articleType : props.mediaType} content
+                </h3>
+            </div>
             {props.section.map((section: any, index: number) => {
                 const containerProps = {
                     section: props.section,
@@ -23,74 +35,72 @@ export default function SectionContainerEditor(props: any) {
                 }
             })}
             <div className="w-[60px] mx-auto">
-                <Dropdown>
-                    <DropdownTrigger>
-                        <Button
-                            variant="solid"
-                            color="primary">
-                            +
-                        </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu
-                        onAction={(event) => {
-                            let section = {};
-                            switch (event) {
-                                case "image":
-                                    section = {
-                                        type: event,
-                                        imageUrl: "",
-                                        width: 0,
-                                        height: 0,
-                                        altText: "",
-                                        caption: "",
-                                        credit: "",
-                                        pos: "center",
-                                        wrap: "true",
-                                    };
-                                    break;
-                                case "tableGraph":
-                                    section = {
-                                        type: event,
-                                        table: {
-                                            type: "Bar",
-                                            headings: {
-                                                axis: { x: "X", y: "Y" },
-                                                rows: ["Row 1", "Row 2", "Row 3"],
-                                                cols: ["Col 1", "Col 2"],
-                                            },
-                                            colors: ["#109191", "#57adde"],
-                                            data: [
-                                                [0, 0],
-                                                [0, 0],
-                                                [0, 0],
-                                            ],
-                                            references: [
-                                                {
-                                                    label: "Ref 1",
-                                                    y: 0,
-                                                    x: null,
-                                                    color: "#109191",
-                                                },
-                                            ],
-                                            display: { table: "true", graph: "true" },
-                                        },
-                                    };
-                                    break;
-                                default:
-                                    section = { type: event, text: "" };
-                                    break;
-                            }
-                            props.setSection([...props.section, section]);
-                        }}
-                        items={[
-                            { name: "Section Header", key: "sectionHeader" },
-                            { name: "Text", key: "text" },
-                            { name: "Image", key: "image" },
-                            { name: "Table/graph", key: "tableGraph" },
-                        ]}>
-                        {(item: any) => <DropdownItem key={item.key}>{item.name}</DropdownItem>}
-                    </DropdownMenu>
-                </Dropdown>
+                <Popup
+                    trigger={<Button color="primary">+</Button>}
+                    position={"bottom center"}>
+                    <div className="popup">
+                        {sectionItems.map((type: any, index: number) => {
+                            return (
+                                <div
+                                    className="popup-item"
+                                    onClick={() => {
+                                        let section = {};
+                                        switch (type.key) {
+                                            case "image":
+                                                section = {
+                                                    type: type.key,
+                                                    imageUrl: "",
+                                                    width: 0,
+                                                    height: 0,
+                                                    altText: "",
+                                                    caption: "",
+                                                    credit: "",
+                                                    pos: "center",
+                                                    wrap: "true",
+                                                };
+                                                break;
+                                            case "tableGraph":
+                                                section = {
+                                                    type: type.key,
+                                                    table: {
+                                                        type: "Bar",
+                                                        headings: {
+                                                            axis: { x: "X", y: "Y" },
+                                                            rows: ["Row 1", "Row 2", "Row 3"],
+                                                            cols: ["Col 1", "Col 2"],
+                                                        },
+                                                        colors: ["#109191", "#57adde"],
+                                                        data: [
+                                                            [0, 0],
+                                                            [0, 0],
+                                                            [0, 0],
+                                                        ],
+                                                        references: [
+                                                            {
+                                                                label: "Ref 1",
+                                                                y: 0,
+                                                                x: null,
+                                                                color: "#109191",
+                                                            },
+                                                        ],
+                                                        display: { table: "true", graph: "true" },
+                                                    },
+                                                };
+                                                break;
+                                            default:
+                                                section = { type: type.key, text: "" };
+                                                break;
+                                        }
+                                        console.log(section);
+
+                                        props.setSection([...props.section, section]);
+                                    }}>
+                                    <p className="inline pl-[4px]">{type.name}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </Popup>
             </div>
         </div>
     );
